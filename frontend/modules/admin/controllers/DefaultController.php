96f183a3415e68d54ce15719cf717f275c289d2c
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2023-08-29 00:16:31 +0500

    sdf

--- frontend/modules/admin/controllers/DefaultController.php
+++ frontend/modules/admin/controllers/DefaultController.php
@@ -3,6 +3,7 @@
 namespace frontend\modules\admin\controllers;
 
 use common\models\Certificate;
+use PhpOffice\PhpWord\Settings;
 use yii\web\Controller;
 use yii\web\UploadedFile;
 use Yii;
@@ -24,15 +25,40 @@ class DefaultController extends Controller
                 $ext = $model->word->extension;
                 $year = date('Y');
                 $month = date('m');
-                mkdir(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month, 0777, true);
+                if(!file_exists(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month)){
+                    mkdir(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month, 0777, true);
+                }
                 $model->word->saveAs(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'.'.$ext);
                 $model->word = $name.'.'.$ext;
 
                 // generate word
-                $phpWord = new \PhpOffice\PhpWord\PhpWord();
+                $domPdfPath = realpath(Yii::$app->vendorPath.'/mpdf/mpdf');
 
-                $phpWord->save(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'.'.$ext,Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'.pdf');
+                Settings::setPdfRendererPath($domPdfPath);
 
+                Settings::setPdfRendererName('MPDF');
+
+                $Content = \PhpOffice\PhpWord\IOFactory::load(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'.'.$ext);
+//                $Content->setDefaultFontName('times new roman');
+                //Save it into PDF
+                $PDFWriter = \PhpOffice\PhpWord\IOFactory::createWriter($Content,'PDF');
+                $PDFWriter->save(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'.pdf');
+
+//                $Content =  new \PhpOffice\PhpWord\TemplateProcessor(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'.'.$ext);
+//                $Content->saveAs(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'-1.'.$ext);
+
+//                Yii::$app->response->sendFile(Yii::$app->basePath.'/web/upload/'.$year.'/'.$month.'/'.$name.'.pdf');
+                $model->word = $year.'/'.$month.'/'.$name.'.'.$ext;
+                $model->pdf = $year.'/'.$month.'/'.$name.'.pdf';
+            }
+
+            if($model->save()){
+                Yii::$app->session->setFlash('success','Fayl muvoffaqiyatli saqlandi');
+
+                return $this->redirect(['/admin/certificate/view','id'=>$model->id]);
+            }else{
+                Yii::$app->session->setFlash('error','Faylni saqlashda xatolik');
+                return $this->redirect(['index']);
             }
 
         }
